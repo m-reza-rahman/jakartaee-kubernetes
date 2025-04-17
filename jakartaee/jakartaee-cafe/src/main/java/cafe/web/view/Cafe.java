dd96b7f62f9d46076f99acc93fd89ac896b5bd5f
Reza Rahman <reza_rahman@mail.com>
2025-04-17 16:16:41 -0400

    Fix bugs

--- jakartaee/jakartaee-cafe/src/main/java/cafe/web/view/Cafe.java
+++ jakartaee/jakartaee-cafe/src/main/java/cafe/web/view/Cafe.java
@@ -7,9 +7,7 @@ import java.util.List;
 import cafe.model.entity.Coffee;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.SessionScoped;
-import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -24,15 +22,15 @@ import jakarta.ws.rs.core.MediaType;
 public class Cafe implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private static final String BASE_URI = "http://localhost:9080/jakartaee-cafe/rest/coffees";
 
-	private transient String baseUri;
 	private transient Client client;
 
-	@NotBlank
+	@NotBlank(message = "Name can't be blank")
 	private String name;
-	@NotNull
-	@PositiveOrZero
-	private Number price;
+	@NotNull(message = "Price can't be null")
+	@PositiveOrZero(message = "Price can't be negative")
+	private double price;
 
 	public String getName() {
 		return name;
@@ -42,37 +40,32 @@ public class Cafe implements Serializable {
 		this.name = name;
 	}
 
-	public Number getPrice() {
+	public double getPrice() {
 		return price;
 	}
 
-	public void setPrice(Number price) {
+	public void setPrice(double price) {
 		this.price = price;
 	}
 
 	@PostConstruct
 	private void init() {
-		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
-				.getRequest();
-		baseUri = "http://localhost:9080" + request.getContextPath() + "/rest/coffees";
 		client = ClientBuilder.newBuilder().build();
 	}
 
 	public List<Coffee> getCoffeeList() {
-		return client.target(baseUri).path("/").request(MediaType.APPLICATION_JSON)
+		return client.target(BASE_URI).path("/").request(MediaType.APPLICATION_JSON)
 				.get(new GenericType<List<Coffee>>() {
 				});
 	}
 
 	public void addCoffee() throws IOException {
-		Coffee coffee = new Coffee(this.name, this.price.doubleValue());
-		client.target(baseUri).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
-		FacesContext.getCurrentInstance().getExternalContext().redirect("");
+		Coffee coffee = new Coffee(this.name, price);
+		client.target(BASE_URI).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
 	}
 
 	public void removeCoffee(String coffeeId) throws IOException {
-		client.target(baseUri).path(coffeeId).request().delete();
-		FacesContext.getCurrentInstance().getExternalContext().redirect("");
+		client.target(BASE_URI).path(coffeeId).request().delete();
 	}
 
 	// This method is called after de-serialization to initialize transient fields.
