3425c1e9aabbeecd9a44481e2e6c6624153a30e8
Reza Rahman <reza_rahman@mail.com>
2025-04-18 17:33:53 -0400

    Upgrade to EE 10

--- operators/jakartaee-cafe/src/main/java/cafe/model/CafeRepository.java
+++ operators/jakartaee-cafe/src/main/java/cafe/model/CafeRepository.java
@@ -5,40 +5,39 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import cafe.model.entity.Coffee;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @Stateless
 public class CafeRepository {
 
-  private static final Logger logger =
-      Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+	private static final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
-  @PersistenceContext private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-  public List<Coffee> getAllCoffees() {
-    logger.log(Level.INFO, "Finding all coffees.");
+	public List<Coffee> getAllCoffees() {
+		logger.log(Level.INFO, "Finding all coffees.");
 
-    return this.entityManager.createNamedQuery("findAllCoffees", Coffee.class).getResultList();
-  }
+		return this.entityManager.createNamedQuery("findAllCoffees", Coffee.class).getResultList();
+	}
 
-  public Coffee persistCoffee(Coffee coffee) {
-    logger.log(Level.INFO, "Persisting the new coffee {0}.", coffee);
-    this.entityManager.persist(coffee);
-    return coffee;
-  }
+	public Coffee persistCoffee(Coffee coffee) {
+		logger.log(Level.INFO, "Persisting the new coffee {0}.", coffee);
+		this.entityManager.persist(coffee);
+		return coffee;
+	}
 
-  public void removeCoffeeById(Long coffeeId) {
-    logger.log(Level.INFO, "Removing a coffee {0}.", coffeeId);
-    Coffee coffee = entityManager.find(Coffee.class, coffeeId);
-    this.entityManager.remove(coffee);
-  }
+	public void removeCoffeeById(Long coffeeId) {
+		logger.log(Level.INFO, "Removing a coffee {0}.", coffeeId);
+		Coffee coffee = entityManager.find(Coffee.class, coffeeId);
+		this.entityManager.remove(coffee);
+	}
 
-  public Coffee findCoffeeById(Long coffeeId) {
-    logger.log(Level.INFO, "Finding the coffee with id {0}.", coffeeId);
-    return this.entityManager.find(Coffee.class, coffeeId);
-  }
+	public Coffee findCoffeeById(Long coffeeId) {
+		logger.log(Level.INFO, "Finding the coffee with id {0}.", coffeeId);
+		return this.entityManager.find(Coffee.class, coffeeId);
+	}
 }
--- operators/jakartaee-cafe/src/main/java/cafe/model/entity/Coffee.java
+++ operators/jakartaee-cafe/src/main/java/cafe/model/entity/Coffee.java
@@ -2,75 +2,77 @@ package cafe.model.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
 
-@XmlRootElement
 @Entity
 @NamedQuery(name = "findAllCoffees", query = "SELECT o FROM Coffee o")
 public class Coffee implements Serializable {
 
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-  @Id @GeneratedValue private Long id;
-  protected String name;
-  protected Double price;
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Double price;
 
-  public Coffee() {}
+	public Coffee() {
+		// Nothing to do.
+	}
 
-  public Coffee(String name, Double price) {
-    this.name = name;
-    this.price = price;
-  }
+	public Coffee(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public Double getPrice() {
-    return price;
-  }
+	public Double getPrice() {
+		return price;
+	}
 
-  public void setPrice(Double price) {
-    this.price = price;
-  }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  @Override
-  public int hashCode() {
-    int hash = 0;
-    hash += (id != null ? id.hashCode() : 0);
-    return hash;
-  }
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (id != null ? id.hashCode() : 0);
+		return hash;
+	}
 
-  @Override
-  public boolean equals(Object object) {
-    if (!(object instanceof Coffee)) {
-      return false;
-    }
-    Coffee other = (Coffee) object;
-    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-      return false;
-    }
-    return true;
-  }
+	@Override
+	public boolean equals(Object object) {
+		if (!(object instanceof Coffee)) {
+			return false;
+		}
+		Coffee other = (Coffee) object;
+		if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+			return false;
+		}
+		return true;
+	}
 
-  @Override
-  public String toString() {
-    return "cafe.model.entity.Coffee[id=" + id + ", name=" + name + ", price=" + price + "]";
-  }
+	@Override
+	public String toString() {
+		return "cafe.model.entity.Coffee[id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
 }
